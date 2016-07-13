bc16482ba8e077443a949913090c540eb88febf8
Nicolaos Moscholios <nmoscholios@gmail.com>
2016-07-13 15:37:51 +0200

    Created object for Interface
    Revert to this if splitting up Model, Engine etc don’t work!

--- static/threejs/utils.js
+++ static/threejs/utils.js
@@ -2,16 +2,55 @@
  * Created by Nicola on 13/07/16.
  */
 
-// used for the loading of tranlations/animations, modifies css
+// object to set the front end stuff
+function Button(name,icon1, icon2){
+    this.play_icon = icon1;
+    this.pause_icon = icon2;
+    this.name = name;
+    this.play_on = true;
+}
+
+Button.prototype = {
+    constructor: Button,
     
-var Spinner = (function (div_id) {
-    this.div_id = div_id;
+    play: function(){
+        // set the icon for the button
+        document.getElementById(this.name).className = "glyphicon glyphicon-"+this.play_icon;
+        this.play_on = true;
+    },
+    pause: function(){
+        // set the icon for the button
+        document.getElementById(this.name).className = "glyphicon glyphicon-"+this.pause_icon;
+        this.play_on = false;
+    },
+    switch: function(){
+        if (this.play_on){
+            this.pause();
+        }
+        else{
+            this.play();
+        }
+    }
+};
 
-    this.enable = function(){
-        document.getElementById(this.div_id).style.visibility = 'visible';
-    };
-    this.disable = function(){
-        document.getElementById(this.div_id).style.visibility = 'hidden';
-    };
-});
+// used for the loading of tranlations/animations, modifies css
+function INTERFACE() {
+    this.play_pause_button = new Button('switcher','play', 'pause');
+}
 
+INTERFACE.prototype = {
+
+    constructor: INTERFACE,
+
+    enableSpinner: function(div) {
+        console.log('enabling');
+        document.getElementById(div).style.visibility = 'visible';
+    },
+    disableSpinner: function(div){
+        console.log('disabling');
+        document.getElementById(div).style.visibility = 'hidden';
+    },
+    setGloss: function(div, text) {
+        document.getElementById(div).innerHTML = text;
+    }
+};
