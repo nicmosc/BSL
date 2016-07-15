076321dfbac3e4f3f8e7269079a2349ae2ea49b4
Nicolaos Moscholios <nmoscholios@gmail.com>
2016-07-15 13:26:09 +0200

    Removed nltk from setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ setup(name='UsingSessions', version='1.0',
       #  Uncomment one or more lines below in the install_requires section
       #  for the specific client drivers/modules your application needs.
       install_requires=['flask',
-                        'nltk'
                         #  'MySQL-python',
                         #  'pymongo',
                         #  'psycopg2',
